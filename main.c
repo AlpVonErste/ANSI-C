448967964eb985e673ab1b552773d251408ee032
Alperen Tunc <115324004+alperen-tunc@users.noreply.github.com>
2022-10-14 09:50:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+rechteck1(int breite, int hoehe)
+{
+    for(int i=0; i<hoehe; i++)
+    {
+        for(int j=0; j<breite; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+rechteckRekursive(int hoehe, int breite, int b)
+{
+    if(b>0)
+    {
+        printf("*");
+        rechteckRekursive(hoehe, breite, b-1);
+    }
+    else
+    {
+        if(hoehe > 1)
+        {
+            printf("\n");
+            rechteckRekursive(hoehe-1, breite, breite);
+        }
+    }
+}
+
+rechteck(int h, int b)
+{
+    rechteckRekursive(h,b,b);
+}
+
+int main()
+{
+    int breite, hoehe;
+
+    printf("Geben Sie bitte Breite ein: ");
+    fflush(stdin);
+    scanf("%d", &breite);
+
+    printf("Geben Sie bitte Hoehe ein: ");
+    fflush(stdin);
+    scanf("%d", &hoehe);
+
+    rechteck(hoehe, breite);
+
+    return 0;
+}
